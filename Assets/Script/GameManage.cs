ee651046551a3c48aa7ff8f5cbb9bcf189afe270
jagustinus <jagustinus@student.ciputra.ac.id>
2021-01-14 20:53:27 +0700

    initial commit

---
+++ Assets/Script/GameManage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManage : MonoBehaviour
+{
+    public GameObject GameCanvas;
+    public GameObject MainCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 0;
+        MainCanvas.SetActive(true);
+    }
+}
---
+++ Assets/Script/knight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knight : MonoBehaviour
+{
+    public GameObject plane1;
+    public GameObject plane2;
+    public GameManage gameManager;
+    private Rigidbody2D rb;
+    int plane = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane1.gameObject.SetActive(true);
+        plane2.gameObject.SetActive(false);
+    }
+    public void Switch()
+    {
+        switch(plane)
+        {
+            case 1:
+                plane = 2;
+                plane1.gameObject.SetActive(false);
+                plane2.gameObject.SetActive(true);
+                break;
+            case 2:
+                plane = 1;
+                plane1.gameObject.SetActive(true);
+                plane2.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
